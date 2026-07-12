7053fca25c7c6bf76a05844d82c0165aba61a991
kentthang010 <thangkent010@gmail.com>
2026-07-12 23:43:53 +0200

    add tailwind and tweak frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -42,26 +42,28 @@ function App() {
 			</div>
 			<div className="dashboard">
 				<h1>mayak</h1>
-				<p>Activity Calendar</p>
-				<div className="heatmap">
-					<CalendarHeatmap
-						startDate={oneYearAgo}
-						endDate={today}
-						values={dashboard?.heatmap ?? []}
-						classForValue={(value) => {
-							if (!value) {
-								return 'color-empty'
-							} else if (value.count > 0) {
-								return `color-github-1`
-							} else if (value.count > 10) {
-								return `color-github-2`
-							} else if (value.count > 20) {
-								return `color-github-3`
-							} else if (value.count > 30) {
-								return `color-github-4`
-							} else return `color-github-5`
-						}}
-					/>
+				<div className="pt-1">
+					<p className="text-xl">Activity Calendar</p>
+					<div className="heatmap">
+						<CalendarHeatmap
+							startDate={oneYearAgo}
+							endDate={today}
+							values={dashboard?.heatmap ?? []}
+							classForValue={(value) => {
+								if (!value) {
+									return 'color-empty'
+								} else if (value.count > 0) {
+									return `color-github-1`
+								} else if (value.count > 10) {
+									return `color-github-2`
+								} else if (value.count > 20) {
+									return `color-github-3`
+								} else if (value.count > 30) {
+									return `color-github-4`
+								} else return `color-github-5`
+							}}
+						/>
+					</div>
 				</div>
 				<p>
 					monkeytype tests today: {dashboard?.monkeytype.testsToday}
